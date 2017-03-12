2446bd3c08a5be3fa8eae0592f27e3184fe4d9a0
Chihiro Shimmur <chihiros92@suou.waseda.jp>
2017-03-12 18:28:55 +0900

    Add OpenCV study files

---
+++ opencv-time/basic/multiChannelDepth/cmake-build-debug/CMakeFiles/multiChannelDepth.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/multiChannelDepth.dir/main.cpp.o"
+  "multiChannelDepth.pdb"
+  "multiChannelDepth"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/multiChannelDepth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
